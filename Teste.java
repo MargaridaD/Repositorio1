9d42a0f650f4faa4352a46e1c316b79d73ee4e1d
MargaridaD <97111622+MargaridaD@users.noreply.github.com>
2022-01-04 15:35:41 +0000

    Soma
    Acrescentada Função Soma

---
+++ Teste.java
@@ -0,0 +1,16 @@
+public class Teste
+{
+
+  public static void main(String args[])
+  {
+  
+		public static int somarNumeros(int aNumero1,int aNumero2);
+		  {
+		      int soma = aNumero1 + aNumero2;
+		    }
+			
+		    return soma;
+		  }
+	  
+  }
+}
